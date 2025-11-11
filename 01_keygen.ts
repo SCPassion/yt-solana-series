6679965a5674a096298aac17bc23e322d4f69ce4
scp <bernard.cryptohk@gmail.com>
2025-11-11 21:22:49 +0100

    Update package.json and pnpm-lock.yaml to add dotenv and esrun dependencies

---
+++ 01_keygen.ts
@@ -0,0 +1,8 @@
+import { Keypair } from "@solana/web3.js";
+
+const keypair = Keypair.generate(); // generate a fresh keypair
+
+// convert to 58 base string for human readability
+console.log(`This is the public key: ${keypair.publicKey.toBase58()}`);
+console.log(`This is the secret key: ${keypair.secretKey}`); // store it to .env file. so that we can use it in the next steps.
+console.log(`key pair: ${keypair}`);
